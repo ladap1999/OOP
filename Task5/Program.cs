df6c09d1b2c26a97214a588e0681f46edd0723f8
ladap1999 <ladap1999>
2023-04-16 23:11:35 +0300

    add project Task5.csproj

---
+++ Task5/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Polynomial polynomial = new Polynomial(-1, 2, -6, 8, -3);
+            Console.WriteLine(polynomial.ToString());
+        }
+    }
+}
